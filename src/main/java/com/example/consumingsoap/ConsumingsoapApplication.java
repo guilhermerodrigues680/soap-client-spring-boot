aba835835b8c24faf22f66fd961a299ddb055604
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-01-21 20:58:59 -0300

    Commit inicial

---
+++ src/main/java/com/example/consumingsoap/ConsumingsoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumingsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingsoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingsoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consumingsoap/ConsumingsoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingsoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
